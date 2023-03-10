20e5beaf13ca547a40884a1ef1ea27c767a3f1c8
Chstphr <somto565@gmail.com>
2023-03-10 19:05:17 +0100

    done with the search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div>Search</div>
+    <div className='search'>
+      <div className="searchForm">
+        <input type="text" placeholder='Find A User'/>
+      </div>
+      <div className="userChat">
+        <img src="https://images.pexels.com/photos/6279415/pexels-photo-6279415.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" />
+        <div className="userChatInfo">
+          <span>Jane</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
