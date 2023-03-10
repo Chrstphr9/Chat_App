e410d4084e12d970b73f293fe0d21f087283ad5d
Chstphr <somto565@gmail.com>
2023-03-10 09:40:03 +0100

    done with login lol

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Add from "../images/addAvatar.png"
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='formContainer'>
+        <div className="formWrapper">
+            <span className="logo">Lama Chat</span>
+            <span className="title">Login</span>
+            <form>
+                <input type="email" placeholder='email' />
+                <input type="email" placeholder='email'/>
+                <input type="password" placeholder='password'/>
+               
+                <button>Sign In</button>
+            </form>
+            <p>You Don't Have An Account? Register</p>
+        </div>
+    </div>
   )
 }
 
