37b8a5b1c7e17d084b1bb2ae6fe03e4d74ebe348
Chstphr <somto565@gmail.com>
2023-03-10 09:12:37 +0100

    created image folder

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -10,7 +10,10 @@ const Register = () => {
                 <input type="text" placeholder='Display Name' />
                 <input type="email" placeholder='email'/>
                 <input type="password" placeholder='password'/>
-                <input type="file" />
+                <input style={{display:'none'}} type="file" id='file'/>
+                <label htmlFor="file">
+                    <img src="" alt="" />
+                </label>
                 <button>Sign Up</button>
             </form>
             <p>You Do Have An Account</p>
