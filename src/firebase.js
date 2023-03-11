704b09c03e1c69049fbf4d9ca77168a6a0e73548
Chstphr <somto565@gmail.com>
2023-03-11 16:25:52 +0100

    pasted firebase config

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCFlJAzUlc22X9jhuAJMZ83dLbmTtgQKkc",
+  authDomain: "chat-app-70855.firebaseapp.com",
+  projectId: "chat-app-70855",
+  storageBucket: "chat-app-70855.appspot.com",
+  messagingSenderId: "579716191220",
+  appId: "1:579716191220:web:00f73121577fd697d69fa1"
+};
+
+
+const app = initializeApp(firebaseConfig);
