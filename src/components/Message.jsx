bb134f95d0979d6474601c264bdc452be1ab88d7
Chstphr <somto565@gmail.com>
2023-03-11 15:54:28 +0100

    still coding

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Message = () => {
   return (
-    <div className="message">
+    <div className="message owner">
       <div className="messageInfo">
         <img
           src="https://images.pexels.com/photos/6279415/pexels-photo-6279415.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
