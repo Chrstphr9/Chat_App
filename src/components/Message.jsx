0852a94d4db92499a61c550c394c1ceacf7ec82a
Chstphr <somto565@gmail.com>
2023-03-11 10:38:12 +0100

    done with message jsx

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Message = () => {
   return (
-    <div>Message</div>
-  )
-}
+    <div className="message">
+      <div className="messageInfo">
+        <img
+          src="https://www.pexels.com/photo/young-woman-in-casual-clothing-14711714/"
+          alt=""
+        />
+        <span>Just Now</span>
+      </div>
+      <div className="messageContent">
+        <p>Hello</p>
+        <img src="https://www.pexels.com/photo/young-woman-in-casual-clothing-14711714/" alt="" />
+      </div>
+    </div>
+  );
+};
 
-export default Message
+export default Message;
