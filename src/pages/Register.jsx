13115365380b17214f1998bfd4a0f0e0433641cf
Chstphr <somto565@gmail.com>
2023-03-13 08:10:16 +0100

    able to save user

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -12,26 +12,29 @@ const Register = () => {
     const password = e.target[2].value;
     const file = e.target[3].files[0];
 
-    // const auth = getAuth();
-createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
+    // const auth = getAuth(); 
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      
+
+      console.log(user);
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // ..
+    });
   }
 
   return (
     <div className='formContainer'>
         <div className="formWrapper">
-            <span className="logo">Lama Chat</span>
+            <span className="logo">Somto Chat</span>
             <span className="title">Register</span>
-            <form onSubmit={handleSubmit }>
+            <form onSubmit={handleSubmit}>
                 <input type="text" placeholder='Display Name' />
                 <input type="email" placeholder='email'/>
                 <input type="password" placeholder='password'/>
@@ -42,7 +45,7 @@ createUserWithEmailAndPassword(auth, email, password)
                 </label>
                 <button>Sign Up</button>
             </form>
-            <p>You Do Have An Account</p>
+            <p>You Do Have An Account? Login</p>
         </div>
     </div>
   )
