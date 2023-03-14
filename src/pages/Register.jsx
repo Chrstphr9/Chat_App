daa3dafcfcfc8e1b0b0a558628455a93b11f52ec
Chstphr <somto565@gmail.com>
2023-03-14 09:47:37 +0100

    display name function

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,51 +1,70 @@
-import React from 'react'
-import { useState } from 'react';
-import Add from "../images/addAvatar.png"
-import {  createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase"
+import React from "react";
+import { useState } from "react";
+import Add from "../images/addAvatar.png";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, storage } from "../firebase";
+import {
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
 
 const Register = () => {
-  const [err, setErr] = useState(false)
+  const [err, setErr] = useState(false);
   const handleSubmit = async (e) => {
-    
-    e.preventDefault()
+    e.preventDefault();
     const displayName = e.target[0].value;
     const email = e.target[1].value;
     const password = e.target[2].value;
     const file = e.target[3].files[0];
 
     try {
-      const res = await createUserWithEmailAndPassword(auth, email, password)
-    }
-    catch(err) {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+
+      const storageRef = ref(storage, displayName);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+     
+        (error) => {
+          setErr(true);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
+            await updateProfile(res.user,{
+              displayName,
+              photoURL:downloadURL
+            });
+          });
+        }
+      );
+    } catch (err) {
       setErr(true);
-
     }
-    
-
-  }
+  };
 
   return (
-    <div className='formContainer'>
-        <div className="formWrapper">
-            <span className="logo">Somto Chat</span>
-            <span className="title">Register</span>
-            <form onSubmit={handleSubmit}>
-                <input type="text" placeholder='Display Name' />
-                <input type="email" placeholder='email'/>
-                <input type="password" placeholder='password'/>
-                <input style={{display:'none'}} type="file" id='file'/>
-                <label htmlFor="file">
-                    <img src={Add} alt="" />
-                    <span>Add An Avatar</span>
-                </label>
-                <button>Sign Up</button>
-                {err && <span>Something Went Wrong</span> }
-            </form>
-            <p>You Do Have An Account? Login</p>
-        </div>
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Somto Chat</span>
+        <span className="title">Register</span>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Display Name" />
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <input style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <img src={Add} alt="" />
+            <span>Add An Avatar</span>
+          </label>
+          <button>Sign Up</button>
+          {err && <span>Something Went Wrong</span>}
+        </form>
+        <p>You Do Have An Account? Login</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
