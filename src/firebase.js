125005c8e39a1ea2704a59fdbf37f135db8a5173
Chstphr <somto565@gmail.com>
2023-03-14 09:11:59 +0100

    imported and defined the storage

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"
+import { getAuth } from "firebase/auth";
+import { getStorage, ref } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCFlJAzUlc22X9jhuAJMZ83dLbmTtgQKkc",
@@ -15,3 +16,4 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig)
 export const auth = getAuth()
+export const storage = getStorage();
